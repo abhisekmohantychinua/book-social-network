a66c2346f7267e7db6959424914a620cf361d33f
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-04-18 20:09:00 +0530

    Initial commit for api

---
+++ book-network-api/src/test/java/dev/abhisek/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abhisek.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
